8347389c09b17251674fae1215ce57a2bf21bc3e
Hiroki MATSUMOTO <e125705@ie.u-ryukyu.ac.jp>
2013-11-17 14:30:27 +0900

    make PakuriBook dir

---
+++ PakuriBook/PakuriBook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PakuriBook
+//
+//  Created by Hiroki MATSUMOTO on 2013/11/17.
+//  Copyright (c) 2013年 Hiroki MATSUMOTO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
