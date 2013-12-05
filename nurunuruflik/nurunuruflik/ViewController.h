aad1aa11fc417babccd743f914b76f881eb9fb06
Hiroki MATSUMOTO <e125705@ie.u-ryukyu.ac.jp>
2013-12-05 11:52:26 +0900

    add change2

--- nurunuruflik/nurunuruflik/ViewController.h
+++ nurunuruflik/nurunuruflik/ViewController.h
@@ -11,6 +11,7 @@
 @interface ViewController : UIViewController
 
 
+- (IBAction)start:(id)sender;
 
 
 @end
--- nurunuruflik/nurunuruflik/ViewController.m
+++ nurunuruflik/nurunuruflik/ViewController.m
@@ -27,4 +27,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)start:(id)sender {
+    
+    
+}
 @end
