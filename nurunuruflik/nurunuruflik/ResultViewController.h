2819abb19fc06606467ee49a68c39fc1c24fad8b
Hiroki MATSUMOTO <e125705@ie.u-ryukyu.ac.jp>
2014-01-14 18:13:09 +0900

    create ResultViewController

---
+++ nurunuruflik/nurunuruflik/ResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultViewController.h
+//  nurunuruflik
+//
+//  Created by Hiroki MATSUMOTO on 1/14/14.
+//  Copyright (c) 2014 Hiroki MATSUMOTO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultViewController : UIViewController
+
+@end
---
+++ nurunuruflik/nurunuruflik/ResultViewController.m
@@ -0,0 +1,38 @@
+//
+//  ResultViewController.m
+//  nurunuruflik
+//
+//  Created by Hiroki MATSUMOTO on 1/14/14.
+//  Copyright (c) 2014 Hiroki MATSUMOTO. All rights reserved.
+//
+
+#import "ResultViewController.h"
+
+@interface ResultViewController ()
+
+@end
+
+@implementation ResultViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
